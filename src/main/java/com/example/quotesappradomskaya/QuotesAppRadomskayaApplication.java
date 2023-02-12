4beae7fae3b26665d890069a89b195b278971e56
EDyugaeva <ed2408@yandex.ru>
2023-02-12 21:43:41 +0500

    Initial commit

---
+++ src/main/java/com/example/quotesappradomskaya/QuotesAppRadomskayaApplication.java
@@ -0,0 +1,13 @@
+package com.example.quotesappradomskaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesAppRadomskayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesAppRadomskayaApplication.class, args);
+	}
+
+}
